78892d91632c8c372e65e48ddd77c5c37d731fb1
Your Name <anujkumar@Anujs-Mac-mini.local>
2026-05-08 13:48:47 +0530

    perf: convert legacy images to WebP, add caching headers, and inject image dimensions to minimize CLS and LCP

--- js/seo-loader.js
+++ js/seo-loader.js
@@ -152,7 +152,7 @@
           config.marqueeKeywords.forEach((keyword) => {
             newHTML += `
               <div class="text">
-                <img class="rotate" src="images/common/shapes/marque-shap.png" alt="">
+                <img class="rotate" src="images/common/shapes/marque-shap.webp" alt="">
                 <h2 class="title">${keyword}</h2>
               </div>
             `;
